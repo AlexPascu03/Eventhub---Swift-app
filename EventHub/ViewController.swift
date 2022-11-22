ed620eb3fb7899aafc371ff57431b2dbe153fba1
Alex Pascu <alexpascu0212@gmail.com>
2022-11-22 12:18:14 +0200

    Initial Commit

---
+++ EventHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventHub
+//
+//  Created by David Alexandru Mihai Pascu on 22.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
